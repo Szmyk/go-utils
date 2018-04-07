c2129f0a63c66e53ce2fddb931d0bc8b24c7bbe6
Szymon Zak <szymonszmykzak@gmail.com>
2018-04-07 18:23:29 +0200

    Update and rename readFile.go to files.go

--- files/files.go
+++ files/files.go
@@ -29,3 +29,20 @@ func readFile(path string) string {
 
 	return content
 }
+
+func writeLines(lines []string, path string) error {
+	file, err := os.Create(path)
+
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+
+	return w.Flush()
+}
